68a03c44359bb5ce32b25437b1283bbf8b8b0d08
Theodor <100497431+theodor8@users.noreply.github.com>
2023-10-12 15:41:44 +0200

    Update ÅngstRooms.py

--- ÅngstRooms.py
+++ ÅngstRooms.py
@@ -2,9 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 
 
-req = "https://cloud.timeedit.net/uu/web/schema/ri1X300ZZ9005QQQ0fZ67o6Y00yQ5YcfYQ0f7Y662v5QX109Y7ZZ0_hapQjxxy_ncdlllyrxwbnaxdb.html"
-htmlDoc = requests.get(req)
+def getHtml(url):
+    htmlDoc = requests.get(url)
+    htmlString = BeautifulSoup(htmlDoc.content, features="html.parser")
+    return htmlString
 
-htmlString = BeautifulSoup(htmlDoc.content, features="html.parser")
 
-print(htmlString.prettify("utf-8"))
+html = getHtml("https://cloud.timeedit.net/uu/web/schema/ri1X300ZZ9005QQQ0fZ67o6Y00yQ5YcfYQ0f7Y662v5QX109Y7ZZ0_hapQjxxy_ncdlllyrxwbnaxdb.html")
+print(html.prettify("utf-8"))
