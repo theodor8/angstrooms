00f73af072a78450c03585514be8ff54e41c0b02
Theodor <100497431+theodor8@users.noreply.github.com>
2023-10-19 14:24:30 +0200

    Update ångstrooms.py

--- ångstrooms.py
+++ ångstrooms.py
@@ -5,7 +5,7 @@ def getSoup(url):
     r = requests.get(url)
     return BeautifulSoup(r.content, "html.parser")
 
-def getText(soup):
+def getBookingText(soup):
     table = soup.find('table', {'class' : 'restable'})
     rows = table.find_all('tr')
     rows = rows[2:]
@@ -28,24 +28,38 @@ def formatBooking(booking):
         room = formatRoom(booking[booking.index('Bokningsbar') - 1])
     return [start, end, room]
 
-def getBookings(date, time, text): # returnar bokade grupprum för datumet och tiden
-    bookings = []
+def getAllBookings(date, text): # returnar alla bokningar för ett visst datum
+    rooms = []
     add = False
     for line in text:
         if add:
-            if 'Ångström' not in line:
-                return bookings
-            booking = formatBooking(line)
-            if booking[0] <= time <= booking[1]:
-                bookings.append(booking)
+            if 'Ångström' not in line: return rooms
+            rooms.append(formatBooking(line))
         if not add and date in line:
             add = True
-        
 
-url = r'https://cloud.timeedit.net/uu/web/schema/ri1X00gX8560Y8QQ8YZ985YY08y5009605X60Q560f543Z485694594558498050860560Y9554YYX8005878088Y84X086X96X506906X602008XY05166053991X50Y4295608635YY845695530YX5Y6755X6655X69X9856667X36Y65579955850995555Y036YX45605X9599057665X7856X535Y88079YY03Y2652X965665454525X936521Y050653569Y5060X35Y556X56XY6553963599Y4506X4650366951XY356069X50506692XY56069Y3656Y5X668915017805614Y8649Y7597X9066X4586555QY.html'
+def getAvailableRooms(allRooms, allBookings, time): # returnerar lediga rum och nästa bokningstid
+    bookedRooms = [booking[2] for booking in allBookings if booking[0] <= time < booking[1]]
+    availableRooms = [room for room in allRooms if room not in bookedRooms]
+    rooms = []
+    for room in availableRooms:
+        for booking in allBookings:
+            if room == booking[2] and time < booking[0]:
+                rooms.append([room, booking[0]])
+                break
+        else: # om det inte finns en nästa bokning, om den inte breakar
+            rooms.append([room, 0])
+    return rooms
+
+ 
+url = r'https://cloud.timeedit.net/uu/web/schema/ri1Xc0gw8560YbQQY7ZgZZZX89ZZZX8Y0Cy300pZZZX8a5Y63Q562f5Y7ZZZZX881B6Cvy5G7Yv7yYYd5Yad7Y655agQ7.html'
+allRooms = [101154, 101156, 101168, 101170, 101174, 101180, 101182, 101192, 10131, 10133, 10205, 10207, 10208, 10210, 10211, 10212, 10213, 10214, 10215, 1403, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 80412, 90402, 90403, 90409]
+date = '2023-10-18'
+time = 1630
+
 soup = getSoup(url)
-text = getText(soup)
+text = getBookingText(soup)
+allBookings = getAllBookings(date, text)
+available = getAvailableRooms(allRooms, allBookings, time)
 
-bookings = getBookings('2023-10-16', 1100, text)
-
-print(bookings)
+print(available)
