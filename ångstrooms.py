0ef88e1ddd6e8c827dd7f2392b05f471932846e5
niklas-04 <144360807+niklas-04@users.noreply.github.com>
2023-10-24 11:26:41 +0200

    Update ångstrooms.py

--- ångstrooms.py
+++ ångstrooms.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import requests
 from bs4 import BeautifulSoup
 
@@ -28,38 +29,82 @@ def formatBooking(booking):
         room = formatRoom(booking[booking.index('Bokningsbar') - 1])
     return [start, end, room]
 
-def getAllBookings(date, text): # returnar alla bokningar för ett visst datum
+def getBookedRooms(date, time, text): # returnar bokade grupprum för datumet och tiden
     rooms = []
     add = False
     for line in text:
         if add:
-            if 'Ångström' not in line: return rooms
-            rooms.append(formatBooking(line))
+            if 'Ångström' not in line:
+                return rooms
+            booking = formatBooking(line)
+            if booking[0] <= time <= booking[1]:
+                rooms.append(booking[2])
         if not add and date in line:
             add = True
 
-def getAvailableRooms(allRooms, allBookings, time): # returnerar lediga rum och nästa bokningstid
-    bookedRooms = [booking[2] for booking in allBookings if booking[0] <= time < booking[1]]
-    availableRooms = [room for room in allRooms if room not in bookedRooms]
-    rooms = []
-    for room in availableRooms:
-        for booking in allBookings:
-            if room == booking[2] and time < booking[0]:
-                rooms.append([room, booking[0]])
-                break
-        else: # om det inte finns en nästa bokning, om den inte breakar
-            rooms.append([room, 0])
-    return rooms
+
+def getAvailableRooms(allRooms, bookedRooms):
+    return [room for room in allRooms if room not in bookedRooms]
 
  
-url = r'https://cloud.timeedit.net/uu/web/schema/ri1Xc0gw8560YbQQY7ZgZZZX89ZZZX8Y0Cy300pZZZX8a5Y63Q562f5Y7ZZZZX881B6Cvy5G7Yv7yYYd5Yad7Y655agQ7.html'
-allRooms = [101154, 101156, 101168, 101170, 101174, 101180, 101182, 101192, 10131, 10133, 10205, 10207, 10208, 10210, 10211, 10212, 10213, 10214, 10215, 1403, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 80412, 90402, 90403, 90409]
-date = '2023-10-18'
-time = 1630
 
-soup = getSoup(url)
-text = getBookingText(soup)
-allBookings = getAllBookings(date, text)
-available = getAvailableRooms(allRooms, allBookings, time)
 
-print(available)
+
+list = [101154, 101156, 101168, 101170, 101174, 101180, 101182, 101192, 10131, 10133, 10205, 10207, 10208, 10210, 10211, 10212, 10213, 10214, 10215, 1403, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 8041, 9040, 9040, 90409]
+
+
+# Funktionen returnernar en funktion eftersom den returnerade funktionen måste sparas i en variabel på line 91
+# För att kunna användas i button command= parametern
+def onButtonClick(root, userDateEntry, userTimeEntry):
+    def returnFunction():
+        global availableRooms
+        date = userDateEntry.get()
+        time = userTimeEntry.get()
+        
+        if date == '' and time == '':
+            return
+        
+        url = r'https://cloud.timeedit.net/uu/web/schema/ri1Xc0gw8560YbQQY7ZgZZZX89ZZZX8Y0Cy300pZZZX8a5Y63Q562f5Y7ZZZZX881B6Cvy5G7Yv7yYYd5Yad7Y655agQ7.html'
+        soup = getSoup(url)
+        text = getBookingText(soup)
+        bookings = getBookedRooms(date,int(time),text)
+        availableRooms = getAvailableRooms(list, bookings)
+        root.quit()
+    return returnFunction
+
+
+def firstWindow():
+    root = tk.Tk()
+    root.geometry('500x250')
+
+    userDateEntry = tk.Entry(root)
+    userDateEntry.grid(row=1,column=1)
+
+    userTimeEntry = tk.Entry(root)
+    userTimeEntry.grid(row=1,column=2)
+
+    dateText = tk.Label(root, text = "Datum (ÅÅÅÅ-MM-DD)")
+    dateText.grid(row=0, column=1)
+
+    timeText = tk.Label(root, text = "Tid (XXXX)")
+    timeText.grid(row=0, column=2)
+
+    onButtonClickWithArgs = onButtonClick(root, userDateEntry, userTimeEntry)
+    dateButton = tk.Button(root, text='Search', command=onButtonClickWithArgs)
+    dateButton.grid(row=1, column=0)
+
+    root.protocol('VM_DELETE_WINDOW', lambda : None)
+    root.mainloop()
+  
+def secondWindow():
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.place(relx=.5, rely=0.5,anchor= 'center')
+    text = tk.Label(frame, text="Lediga rum :")
+    text.grid(row=0, column=0)
+    for room in availableRooms:
+        textLabel = tk.Label(frame, text=str(room))
+        textLabel.grid(row=availableRooms.index(room)+1, column=0)
+    root.mainloop()
+firstWindow()
+secondWindow()
